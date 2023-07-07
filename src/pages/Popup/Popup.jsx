81ae6d10be9af7ae2d29511572f1b3c6c2f1b138
Andrew Glanz <andrewvita0@gmail.com>
2023-07-07 17:57:12 -0400

    Added scrollbar styling

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -20,7 +20,7 @@ const Popup = () => {
           <li>Link 9</li>
           <li>Link 10</li>
           <li>Link 11</li>
-          <li>Link 13</li>
+          <li>Link 12</li>
           <li>Link 13</li>
         </ul>
       </nav>
