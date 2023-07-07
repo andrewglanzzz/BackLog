2776fc8aa30e64d7fdd1563ab3aa66ef8f6a8a15
Andrew Glanz <andrewvita0@gmail.com>
2023-07-07 18:16:20 -0400

    Still working on popup, implementing scrolling.

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -26,6 +26,20 @@ const Popup = () => {
           <li>Link 11</li>
           <li>Link 12</li>
           <li>Link 13</li>
+          <li>Link 11</li>
+          <li>Link 12</li>
+          <li>Link 13</li>
+          <li>Link 10</li>
+          <li>Link 11</li>
+          <li>Link 12</li>
+          <li>Link 13</li>
+          <li>Link 11</li>
+          <li>Link 12</li>
+          <li>Link 13</li>
+          <li>Link 10</li>
+          <li>Link 11</li>
+          <li>Link 12</li>
+          <li>Link 13</li>
         </ul>
       </nav>
     </div>
