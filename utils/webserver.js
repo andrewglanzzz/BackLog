a77dcfb158f7db8e7890fc3f9da1576f52fbbda8
Andrew Glanz <andrewvita0@gmail.com>
2023-07-07 16:58:29 -0400

    Completely refactored project due to errors.

--- utils/webserver.js
+++ utils/webserver.js
@@ -27,7 +27,7 @@ var compiler = webpack(config);
 
 var server = new WebpackDevServer(
   {
-    https: true,
+    https: false,
     hot: true,
     liveReload: false,
     client: {
@@ -40,7 +40,7 @@ var server = new WebpackDevServer(
       directory: path.join(__dirname, '../build'),
     },
     devMiddleware: {
-      publicPath: `https://localhost:${env.PORT}/`,
+      publicPath: `http://localhost:${env.PORT}/`,
       writeToDisk: true,
     },
     headers: {
