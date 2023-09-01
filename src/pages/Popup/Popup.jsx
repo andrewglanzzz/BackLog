a9696a4602e74c300421293b4a5d54358e44f553
Andrew Glanz <andrewvita0@gmail.com>
2023-08-31 22:29:07 -0400

    Added placeholder image

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -109,7 +109,13 @@ const Popup = () => {
           // Handle the response from the content script
           if (response) {
             const { url, albumName, artist, genre, rating } = response;
-            setActiveTabData({ url, albumName, artist, genre, rating });
+            setActiveTabData({
+              url,
+              albumName,
+              artist,
+              genre,
+              rating,
+            });
 
             // Check if the URL is already present in localStorage
             const storedUrlList =
@@ -349,6 +355,7 @@ const Popup = () => {
               {/* Render the URL list with album information in the <ul> element */}
               {displayList.map((data) => (
                 <li key={data.url}>
+                  <img src="https://placehold.co/100x100" width="100"></img>
                   <a href={data.url} target="_blank" rel="noopener noreferrer">
                     {
                       <span className="paddedSpan">
